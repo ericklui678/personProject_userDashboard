052e1ad34af9dff7f08dae40862018e075c1dac1
Erick Lui <erickjin.lui@gmail.com>
2017-08-29 15:29:50 -0700

    changed imports

--- apps/dashboard/urls.py
+++ apps/dashboard/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from __future__ import absolute_import
+from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from __future__ import absolute_import
+from . import models
 
 def index(request):
     # request.session['id'] = 1
